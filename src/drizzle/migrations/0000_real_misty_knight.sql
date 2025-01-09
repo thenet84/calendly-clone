cdb84ebe97aa5c2d9d9b028850099434686082db
thenet84 <the.net.wo@gmail.com>
2025-01-09 13:06:36 +0100

    feat: set up database

---
+++ src/drizzle/migrations/0000_real_misty_knight.sql
@@ -0,0 +1,30 @@
+CREATE TABLE "events" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text NOT NULL,
+	"description" text,
+	"durationInMinutes" integer,
+	"clerkUserId" text NOT NULL,
+	"isActive" boolean NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"updatedAt" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "scheduleAvailabilities" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"scheduleId" uuid NOT NULL,
+	"startTime" text NOT NULL,
+	"endTime" text NOT NULL,
+	"dayOfWeek" "day" NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "schedules" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"timezone" text NOT NULL,
+	"clerkUserId" text NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"updatedAt" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "scheduleAvailabilities" ADD CONSTRAINT "scheduleAvailabilities_scheduleId_schedules_id_fk" FOREIGN KEY ("scheduleId") REFERENCES "public"."schedules"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "clerkUserIdIndex" ON "events" USING btree ("clerkUserId");--> statement-breakpoint
+CREATE INDEX "scheduleIdIndex" ON "scheduleAvailabilities" USING btree ("scheduleId");
